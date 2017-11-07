8cc9bed15f1d04a4ff42a52eb67ab2e1b8c56a5b
Fadi <fadi.josef@turbonomic.com>
2017-11-07 00:29:04 +0000

    Project Files

---
+++ AWS CloudWatch/CloudWatchMonitor/ProjectInstaller.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration.Install;
+using System.Linq;
+using System.ServiceProcess;
+
+
+namespace CloudWatchMonitor
+{
+	[RunInstaller(true)]
+	public partial class ProjectInstaller : System.Configuration.Install.Installer
+	{
+		public ProjectInstaller()
+		{
+			InitializeComponent();
+		}
+
+        private void serviceInstaller1_AfterInstall(object sender, InstallEventArgs e)
+        {
+            ServiceController sc = new ServiceController("AWS CloudWatch");
+            sc.Start();
+
+        }
+
+        private void serviceProcessInstaller1_AfterInstall(object sender, InstallEventArgs e)
+        {
+
+        }
+    }
+}
